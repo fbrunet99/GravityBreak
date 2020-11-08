2b4d43e65ebe0a8091d5e04b6727d8314602b844
fbrunet99 <fred.brunet@gmail.com>
2020-11-08 14:14:12 -0800

    Updated lander animataion from sprite sheet

--- src/ship.gd
+++ src/ship.gd
@@ -1,9 +1,30 @@
 # Ship Area
 extends Node2D
 
+var _throttle
+
 func _input(_event):
-	pass
+	_throttle = Input.get_action_strength("ui_thrust")
+	
+	animate_throttle()
+	if (_throttle > 0):
+		print("throttle = ", _throttle)
 
 
-func thrust():
-	print("thrust")
+func animate_throttle():
+	var level = 0
+	
+	if _throttle > .6:
+		level = 6
+	elif _throttle > .5:
+		level = 5
+	elif _throttle > .4:
+		level = 4
+	elif _throttle > .3:
+		level = 3
+	elif _throttle > .2:
+		level = 1
+	elif _throttle > .1:
+		level = 1
+	
+	$ShipBody/ShipSprite.frame = level
--- src/ship_body.gd
+++ src/ship_body.gd
@@ -1,19 +1,26 @@
+# Ship Body
 extends RigidBody2D
 
 
 var thrust = Vector2(0, -250)
+var throttle = 0
 var torque = 500
 
 func _ready():
 	pass
 
+#func _input(event):
+#	print(event.as_text())
 
 func _integrate_forces(state):
-	
+	throttle = Input.get_action_strength("ui_thrust")
+
 	if Input.is_action_pressed("ui_thrust"):
-		applied_force = thrust.rotated(rotation)
+		applied_force = thrust.rotated(rotation) * throttle
 	else:
 		applied_force = Vector2()
+
+
 	var rotation_dir = 0
 	if Input.is_action_pressed("ui_right"):
 		rotation_dir += 1
